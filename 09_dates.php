dd7857b728fc3c8ea57c59d8774de37611ac9e3b
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-12-20 12:52:18 -0500

    working with dates

--- 09_dates.php
+++ 09_dates.php
@@ -1,14 +1,21 @@
 <?php
 
 // Print current timestamp
+echo time() . "<br>";
 
 // Print current date
+echo date("m-d-y H:i:s A") . "<br>";
 
 // Print yesterday
+echo date("m-d-y H:i:s", time() - 60 * 60 * 24) . "<br>";
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date("F j Y H:i:s A") . "<br>";
 
 // String to timestamp
+$timestamp = strtotime("now") . "<br>";
+
+echo date("F j Y", $timestamp) . "<br>";
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
 
