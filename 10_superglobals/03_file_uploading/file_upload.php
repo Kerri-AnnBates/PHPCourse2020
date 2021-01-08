eff7fee2d726565117c32a1df47afddd97f65b96
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-01-07 21:16:29 -0500

    uploading files to page

--- 10_superglobals/03_file_uploading/file_upload.php
+++ 10_superglobals/03_file_uploading/file_upload.php
@@ -1,19 +1,43 @@
 <?php
 
+$error_message = "";
+
+if (isset($_FILES["file"])) {
+
+    // echo "<pre>";
+    // var_dump($_FILES);
+    // echo "</pre>";
+
+    $file = $_FILES["file"];
+
+    if ($file["type"] === "image/png") {
+        move_uploaded_file($file["tmp_name"], "test.png");
+    } else {
+        $error_message = "You can only upload images.";
+    }
+
+}
+
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
+
 <body>
-<form action="" method="post">
-  <input type="file" name="file"><br>
-  <button>Submit</button>
-</form>
+    <p>Upload an image</p>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="file"><br>
+        <button>Submit</button>
+    </form>
+    <p style="background:firebrick;color:white;"><?php echo isset($error_message) ? $error_message : "" ?></p>
+
+    <img src="./test.png" alt="">
 </body>
+
 </html>
