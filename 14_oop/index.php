539748b32e31b7774d92e650c7db0cdb40b864a0
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-01-27 20:54:58 -0500

    a simple class

--- 14_oop/index.php
+++ 14_oop/index.php
@@ -1,6 +1,16 @@
 <?php
 
 // What is class and instance
+class Person {
+    public $name;
+    public $age;
+    public $salary;
+}
+
+$p = new Person();
+$p->name = "Yoshi";
+$p->$age = 6;
+$p->$salary = null;
 
 // Create Person class in Person.php
 
